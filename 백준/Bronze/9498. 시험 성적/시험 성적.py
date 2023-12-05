0b167903f74d90e9ef12ad240cd7a01c51effc7b
mldlcl22 <152836506+mldlcl22@users.noreply.github.com>
2023-12-05 19:57:55 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,9 @@
+# enter score
+score = int(input())
+
+# result
+if   score >= 90 : print('A')
+elif score >= 80 : print('B')
+elif score >= 70 : print('C')
+elif score >= 60 : print('D')
+else : print('F')
