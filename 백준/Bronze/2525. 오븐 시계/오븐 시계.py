5100b17240bd165c499ebd75c0813f38586c632e
mldlcl22 <152836506+mldlcl22@users.noreply.github.com>
2023-12-05 20:42:45 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,9 @@
+# enter time
+a, b = map(int, input().split())
+c = int(input())
+
+# result
+if b+c < 60 : print(a, b+c)
+else :
+    if a+((b+c)//60) < 24 : print(a+((b+c)//60), (b+c)%60)
+    else : print(a+((b+c)//60)-24, (b+c)%60)
